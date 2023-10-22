3fc42764c35136aa49b5858d9c0cf7a134213bbe
yibing <yo2349@columbia.edu>
2023-10-22 17:30:32 -0400

    update APIs and port.

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ import uvicorn
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
+@app.get("/hello")
+async def hello():
     return {"message": "Hello World"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Awesome cloud developer dff9 says hello {name}"}
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
